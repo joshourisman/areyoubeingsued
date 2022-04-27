77ab990b6326e36478781c09406bd612d0dbd153
Josh Ourisman <me@josho.io>
2022-04-26 20:20:15 -0400

    Removed Google Analytics.

--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,6 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Are you being sued?</title>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-60956755-3', 'auto');
-    ga('send', 'pageview');`,
-          }}
-        />
       </Head>
       <h1 className={styles.zero}>NO*</h1>
       <h1 className={styles.fucks}>* condition of being sued may vary, please consult your attorney</h1>
